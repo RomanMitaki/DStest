67de0f769e12038b820ee120a37e7a66c7d697f4
RomanMitaki <r.mitaki@yandex.ru>
2024-03-18 21:46:27 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import "../../assets/styles/index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <p>Hello world</p>
+      <h1>iedrohgboiaehoiheaoi</h1>
+    </div>
+
+  );
+}
+
+export default App;
